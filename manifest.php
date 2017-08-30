fa268346c7271676a5d4bb895b180ae3d6da2c70
iskawata <kawata@infosign.co.jp>
2017-08-30 22:48:10 +0900

    change manifest.php version number

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
     'label' => 'Super Sports Wagon 2',
     'description' => 'こっちを使う',
     'license' => 'GPL-2.0',
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'taoCe' => '>=3.4.1'
